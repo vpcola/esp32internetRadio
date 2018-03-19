062ac76432dcca0e4abb4edcb9e257b398ec3e21
vpcola <vpcola@gmail.com>
2018-03-19 17:58:16 +0800

    Fixed mDNS errors to compile with latest ESP-IDF

--- components/mdns_task/mdns_task.c
+++ components/mdns_task/mdns_task.c
@@ -25,28 +25,33 @@
 
 void mdns_task(EventGroupHandle_t wifi_event_group)
 {
-    mdns_server_t * mdns = NULL;
+    // mdns_server_t definition is moved to a private include ...
+    // so it not anymore visible
+    //mdns_server_t * mdns = NULL;
+    mdns_txt_item_t items[] = { {"board", "esp32"} };
+
     while(1) {
         /* Wait for the callback to set the CONNECTED_BIT in the
            event group.
         */
         xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT,
                             false, true, portMAX_DELAY);
+        // We go below if we receive a connected_bit event
 
-        if (!mdns) {
             ESP_LOGI(TAG, "starting mDNS");
-            esp_err_t err = mdns_init(TCPIP_ADAPTER_IF_STA, &mdns);
-            if (err) {
-                ESP_LOGE(TAG, "Failed starting MDNS: %u", err);
-                continue;
-            }
 
-            ESP_ERROR_CHECK( mdns_set_hostname(mdns, MDNS_HOSTNAME) );
-            ESP_ERROR_CHECK( mdns_set_instance(mdns, MDNS_INSTANCE) );
+            //esp_err_t err = mdns_init(TCPIP_ADAPTER_IF_STA, &mdns);
+            //if (err) {
+            //    ESP_LOGE(TAG, "Failed starting MDNS: %u", err);
+            //    continue;
+            //}
+            ESP_ERROR_CHECK( mdns_init() );
 
-            ESP_ERROR_CHECK( mdns_service_add(mdns, "_http", "_tcp", 80) );
-            ESP_ERROR_CHECK( mdns_service_instance_set(mdns, "_http", "_tcp", "ESP32 WebServer") );
-        }
+            ESP_ERROR_CHECK( mdns_hostname_set( MDNS_HOSTNAME ) );
+            ESP_ERROR_CHECK( mdns_instance_name_set( MDNS_INSTANCE ) );
+
+            ESP_ERROR_CHECK( mdns_service_add(NULL, "_http", "_tcp", 80, items, 1) );
+            ESP_ERROR_CHECK( mdns_service_instance_name_set("_http", "_tcp", "ESP32 WebServer") );
 
         // sleep 10 seconds
         vTaskDelay(10000 / portTICK_PERIOD_MS);
